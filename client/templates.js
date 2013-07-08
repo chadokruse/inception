070485bf4d50c65c991ad7d1fd2e6f55c10b1b5d
Chad Kruse <chadokruse@gmail.com>
2013-07-08 11:48:19 -0500

    Add Google Analytics

--- client/templates.js
+++ client/templates.js
@@ -187,3 +187,20 @@ Template.action_bar.additional_class = function(order) {
 Template.app.just_added_app = function() {
   return MadewithSession.equals('lastAddedAppName', this.name);
 }
+
+// Google Analytics
+Template.googleAnalytics.rendered = function() {
+    new GA('UA-37513215-1');
+}
+ 
+GA = function(code) {
+    var _gaq = window._gaq || [];
+    _gaq.push(['_setAccount', code]);
+    _gaq.push(['_setDomainName', 'getkyn.com']);
+    _gaq.push(['_trackPageview']);
+    (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+};
