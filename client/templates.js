3fb4ce0bdef295ed9b107d5098cafb8fd6dba4f6
Chad Kruse <chadokruse@gmail.com>
2013-07-08 12:21:11 -0500

    Google Analytics tweak

--- client/templates.js
+++ client/templates.js
@@ -15,6 +15,21 @@ Template.app_list.apps = function() {
   }
 };
 
+// Google Analytics
+Template.ga.rendered = function(){
+    if (window._gaq) return;
+    window._gaq = window._gaq || [];
+    _gaq.push(['_setAccount', 'UA-37513215-1']);
+    _gaq.push(['_setDomainName', 'getkyn.com']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+ }
+
 Template.add_or_draft_app.draft = function() {
   return MadewithSession.get('draft');
 };
@@ -186,21 +201,4 @@ Template.action_bar.additional_class = function(order) {
 
 Template.app.just_added_app = function() {
   return MadewithSession.equals('lastAddedAppName', this.name);
-}
-
-// Google Analytics
-Template.googleAnalytics.rendered = function() {
-    new GA('UA-37513215-1');
-}
- 
-GA = function(code) {
-    var _gaq = window._gaq || [];
-    _gaq.push(['_setAccount', code]);
-    _gaq.push(['_setDomainName', 'getkyn.com']);
-    _gaq.push(['_trackPageview']);
-    (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
 };
