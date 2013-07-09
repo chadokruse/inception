39d7b009410f37c51c7d56982c9a6af0b73625c6
Chad Kruse <chadokruse@gmail.com>
2013-07-08 19:47:57 -0500

    Add Twitter Share Button after idea post [closes #23]

--- client/templates.js
+++ client/templates.js
@@ -196,10 +196,20 @@ Template.action_bar.additional_class = function(order) {
   return MadewithSession.equals('order', order) ? 'sort_toggle_selected' : '';
 };
 
-//Template.install_badge_instructions.just_added_app = function() {
- // return MadewithSession.equals('lastAddedAppName', this.name);
-//};
-
-Template.app.just_added_app = function() {
-  return MadewithSession.equals('lastAddedAppName', this.name);
+Template.install_badge_instructions.just_added_app = function() {
+ return MadewithSession.equals('lastAddedAppName', this.name);
 };
+
+Template.twitter.rendered = function () {
+  //alert(url);
+  !function(d,s,id){
+    var js,
+    fjs=d.getElementsByTagName(s)[0],
+    p=/^http:/.test(d.location)?'http':'https';
+    if(!d.getElementById(id)){js=d.createElement(s);
+      js.id=id;
+      js.src=p+'://platform.twitter.com/widgets.js';
+      fjs.parentNode.insertBefore(js,fjs);
+    }
+  }(document, 'script', 'twitter-wjs');
+}
